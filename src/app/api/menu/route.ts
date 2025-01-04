39361a15a618292529299bf1271f0fc0d0e0f8fa
4ll5olved <abrahamwibowo2@gmail.com>
2025-01-04 20:44:05 +0700

    routing menu to deploy

--- src/app/api/menu/route.ts
+++ src/app/api/menu/route.ts
@@ -1,5 +1,15 @@
 import { menu } from "@/app/data/data";
+import { NextResponse } from "next/server";
 
-export async function GET() {
-    return Response.json(menu);
+export async function GET(
+    request: Request,
+    {params}: {params: {id: string}}
+) {
+    const {id} = await params;
+    const item = menu.find((menuItem) => menuItem.id === parseInt(id));
+    if (!item) {
+        return NextResponse.json({ error: "Item not found" }, { status: 404 });
+      }
+
+    return NextResponse.json(menu);
 }
