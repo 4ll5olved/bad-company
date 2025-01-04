a9d840f5642f48abe0252b8d1e58e6a44d29b1bd
4ll5olved <abrahamwibowo2@gmail.com>
2025-01-04 21:24:14 +0700

    Update route.ts

--- src/app/api/menu/[id]/route.ts
+++ src/app/api/menu/[id]/route.ts
@@ -1,10 +1,18 @@
-import { menu } from "@/app/data/data";
+import { menu } from "@/app/data/data"; // Adjust the import path as necessary
 import { NextResponse } from 'next/server';
 
 export async function GET(
-    _request: Request,
+    request: Request,
     { params }: { params: { id: string } }
 ) {
-    const food = menu.find((item: { id: number }) => item.id.toString() === params.id);
+    // Find the food item by id
+    const food = menu.find((item) => item.id.toString() === params.id);
+
+    // If the food item is not found, return a 404 response
+    if (!food) {
+        return NextResponse.json({ error: "Item not found" }, { status: 404 });
+    }
+
+    // Return the found food item
     return NextResponse.json(food);
 }
