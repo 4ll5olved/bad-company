47560e26ee6f57b60a71de2f6086a09ffda7bcec
4ll5olved <abrahamwibowo2@gmail.com>
2025-01-04 21:21:09 +0700

    try blackcode since Im overheat

--- src/app/api/menu/[id]/route.ts
+++ src/app/api/menu/[id]/route.ts
@@ -1,9 +1,10 @@
 import { menu } from "@/app/data/data";
+import { NextResponse } from 'next/server';
 
 export async function GET(
     _request: Request,
     { params }: { params: { id: string } }
 ) {
-    const food = menu.find((item:{id:number})=>item.id.toString() === params.id);
-    return Response.json(food);
+    const food = menu.find((item: { id: number }) => item.id.toString() === params.id);
+    return NextResponse.json(food);
 }
